7fc363d03323e7ef9acee9f110aef1610ad3b546
Ruben Martinez <rumarfo6@gmail.com>
2022-07-02 12:58:43 -0500

    recover

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,13 +1,45 @@
-import {Link} from 'react-router-dom'
-
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios'
+import Alerta from '../components/Alerta';
 const OlvidePassword = () => {
+
+  const [email,setEmail] = useState('');
+  const [errorForm,setError] = useState({});
+
+  const handleSubmitForm = async e =>{
+     e.preventDefault();
+
+     if(email === ''){ 
+        setError({
+          msg:"El email es Obligatorio",
+          error:true
+        });
+        return;
+     }
+     try{
+       const url = `${import.meta.env.VITE_RUTA}/user/recoverpassword`;
+       const {data} = await axios.post(url,{email});
+       setError({
+        msg: data.msg,
+        error:false
+      });
+     }catch(error){
+      setError({
+        msg: error.response.data.msg,
+        error:true
+      });
+     }
+  }
+
+  const {msg} = errorForm;
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">Recupera tu acceso y no pierdas tus 
       <span className="text-slate-700"> proyectos</span></h1> 
       <form
       className="my-10 br-white shadow rounded-lg p-10"
-       // onSubmit={handleSubmitForm}
+        onSubmit={handleSubmitForm}
       >
         <div className="my-5">
           <label
@@ -19,11 +51,12 @@ const OlvidePassword = () => {
             id="email"
             placeholder="Email de Registro"
             name="email"
-           // value={email}
+            value={email}
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-           // onChange={handlechange}
+            onChange={e => setEmail(e.target.value)}
           />
       </div>
+      {msg && <Alerta alert={errorForm}/>}
       <input
             type="submit"
             className="w-full mb-5 bg-sky-700 text-xl rounded py-3 text-white uppercase
